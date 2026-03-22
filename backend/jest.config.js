7333b421fd05391a615d5aeee16d39f857919f50
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2026-03-22 17:08:05 -0400

    配置分到各自文件夹里去

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,7 +2,7 @@
 export default {
   testEnvironment: "node",
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.test.json" }],
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "<rootDir>/tests/tsconfig.test.json" }],
   },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
